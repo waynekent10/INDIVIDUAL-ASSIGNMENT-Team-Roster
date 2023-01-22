2ca66716e2c2b940b27d067af55fc2f74988cda1
Bruce Clark <bruce.clark55@gmail.com>
2023-01-22 15:07:15 -0600

    added text to home page

--- components/Signin.js
+++ components/Signin.js
@@ -13,8 +13,8 @@ function Signin() {
         margin: '0 auto',
       }}
     >
-      <h1>Hi there!</h1>
-      <p>Click the button below to login!</p>
+      <h1>Welcome to the Page</h1>
+      <p>Click Below to see your Roster</p>
       <Button type="button" size="lg" className="copy-btn" onClick={signIn}>
         Sign In
       </Button>
