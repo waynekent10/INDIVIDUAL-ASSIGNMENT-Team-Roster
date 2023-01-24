2a4721c100bcb9e582d6ba65f132d2050b0741a7
Bruce Clark <bruce.clark55@gmail.com>
2023-01-23 19:34:59 -0600

    View card works, delete and, add player

---
+++ pages/index.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { Button } from 'react-bootstrap';
+import { getTeamMembers } from '../api/teamData';
+import { useAuth } from '../utils/context/authContext';
+import PlayerCard from '../components/PlayerCard';
+
+function Home() {
+  const [members, setMembers] = useState([]);
+
+  const { user } = useAuth();
+
+  const getAllTeamMembers = () => {
+    getTeamMembers(user.uid).then(setMembers);
+  };
+
+  useEffect(() => {
+    getAllTeamMembers();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return (
+    <div className="text-center my-4">
+      <Link href="/team/new" passHref>
+        <Button>Add a Member</Button>
+      </Link>
+      <div className="d-flex flex-wrap">
+        {members.map((member) => (
+          <PlayerCard key={member.firebaseKey} memberObj={member} onUpdate={getAllTeamMembers} />
+        ))}
+      </div>
+
+    </div>
+  );
+}
+export default Home;
---
+++ pages/team/[firebaseKey].js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { Image } from 'react-bootstrap';
+import { ViewMemberDetails } from '../../api/mergedData';
+
+export default function ViewMember() {
+  const [memberDetails, setMemberDetails] = useState({});
+  const router = useRouter();
+
+  const { firebaseKey } = router.query;
+
+  useEffect(() => {
+    ViewMemberDetails(firebaseKey).then(setMemberDetails);
+  }, [firebaseKey]);
+  return (
+    <div className="mt-5 d-flex flex-wrap">
+      <div className="d-flex flex-column">
+        <Image src={memberDetails.image} alt={memberDetails.image} style={{ width: '300px' }} />
+      </div>
+      <div className="text-white ms-5 details">
+        <h5>
+          {memberDetails.name} {memberDetails.role}
+        </h5>
+      </div>
+    </div>
+  );
+}
